ed9dd6f9ca6232568ec3780b6829da5ac9896f88
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-05-25 23:19:45 -0400

    Aula 1 - Atividade 2 - Criando a conexão e executando um statement

---
+++ src/br/com/testes/TestaListagem.java
@@ -0,0 +1,25 @@
+package br.com.testes;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TestaListagem {
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/loja-virtual", "SA", "");
+		Statement statement = connection.createStatement();
+		boolean resultado = statement.execute("select * from Produtos");
+		ResultSet resultSet = (statement).getResultSet();
+		while (resultSet.next()) {
+			int id = resultSet.getInt("id");
+			String nome = resultSet.getString("nome");
+			String descricao = resultSet.getString("descricao");
+			System.out.println(id + nome + descricao);
+		}
+		resultSet.close();
+		statement.close();
+		connection.close();
+	}
+}
