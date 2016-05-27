6caaf830b5ee5350f06288bf1386b28f98e35691
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-05-26 22:23:22 -0400

    Aula 2 - Atividade 2 - Utilizando o ResultSet para pegar o número da chave 'id' gerada

--- src/br/com/testes/TestaInsercao.java
+++ src/br/com/testes/TestaInsercao.java
@@ -13,6 +13,11 @@ public class TestaInsercao {
 		Statement statement = connection.createStatement();
 		boolean result = statement.execute("insert into produtos (nome, descricao) values ('Notebook', 'Notebook i5')", statement.RETURN_GENERATED_KEYS);
 		System.out.println(result);
+		ResultSet resultSet = statement.getGeneratedKeys();
+		while (resultSet.next()) {
+			System.out.println(resultSet.getString("id"));
+		}
+        resultSet.close();	
 		statement.close();
 		connection.close();
 	}
